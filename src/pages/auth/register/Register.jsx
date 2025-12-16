504c786f75898bb35aa7d380ee2b7d10543240d5
Raghad-Moqady <raghadmoqady@gmail.com>
2025-12-16 22:37:38 +0200

    Register Page: handle form using react-hook-form |send request using axios |some edits in register page design

--- src/pages/auth/register/Register.jsx
+++ src/pages/auth/register/Register.jsx
@@ -4,14 +4,32 @@ import zebraImg from "../../../assets/imgs/zebra.webp";
 import rainbowImg from "../../../assets/imgs/rainbow.png";
 import { Box, Button, Card, TextField, Typography } from "@mui/material";
 import SendIcon from '@mui/icons-material/Send';
+import { useForm } from "react-hook-form";
+import axios from "axios";
 
- 
+//  steps:
+/*
+1. design => Done
+2. handle form using react-hook-form , download axios =>Done
+3. client side validation using Yup
+4. server side validation using try, catch (without using react query)
+5. loading 
+6. Alerts
+*/
 export default function Register() {
-  return (
-    <>
-      {/* <img src={zebraImg} alt="" />
-    <img src={rainbowImg} alt="" /> */}
+  const {register, handleSubmit} =useForm({});
 
+  const sendData=async(values)=>{
+      //  console.log(values);
+       try{
+         const response= await axios.post(`https://knowledgeshop.runasp.net/api/Auth/Account/Register`,values);
+         console.log(response);
+       }catch(err){
+         console.log(err);
+       }
+  }
+  return (
+    <> 
       <Box className="ragisterPage" display={"flex"}  alignItems={"center"} justifyContent={"center"} height={"100vh"}>
         {/* img */}
         <Box sx={{ width: "10rem" ,mt:"15rem"}}>
@@ -24,23 +42,23 @@ export default function Register() {
                   src={zebraImg}
                 ></Box>
         </Box> 
-        
+
          {/* form card */}
-        <Box  sx={{width:"40%", py:5 , textAlign:"center",  border: 1, borderColor:'orange' , borderRadius: '16px'}}>
+        <Box sx={{width:"40%", py:5 , textAlign:"center",  border: 1, borderColor:'orange' , borderRadius: '16px'}}>
            <Typography variant='h5' component={"h1"} >Create an Account</Typography>
-           <Box component={"form"} display={"flex"} flexDirection={"column"} gap={2} alignItems={"center"} px={10}>
-              <TextField color="none" type="email" id="email" label="Email" name="email" variant="standard" fullWidth/>
-              <TextField color="none" type="password" id="password" label="Password" name="password" variant="standard"  fullWidth />
-              <TextField color="none" type="text" id="fullName" label="Full Name" name="fullName"  variant="standard"  fullWidth />
-              <TextField color="none" type="text" id="userName" label="User Name" name="userName" variant="standard"  fullWidth />
-              <TextField color="none" type="text" id="phone" label="Phone" name="phoneNumber" variant="standard"  fullWidth />
+           <Box component={"form"} onSubmit={handleSubmit(sendData)} display={"flex"} flexDirection={"column"} gap={2} alignItems={"center"} px={10}>
+              <TextField color="none" type="email" id="email" label="Email" {...register('email')} variant="standard" fullWidth/>
+              <TextField color="none" type="password" id="password" label="Password" {...register('password')} variant="standard"  fullWidth />
+              <TextField color="none" type="text" id="fullName" label="Full Name" {...register('fullName')}  variant="standard"  fullWidth />
+              <TextField color="none" type="text" id="userName" label="User Name" {...register('userName')}  variant="standard"  fullWidth />
+              <TextField color="none" type="text" id="phone" label="Phone" {...register('phoneNumber')}  variant="standard"  fullWidth />
 
               <Button type="submit" variant="contained"  endIcon={<SendIcon/>} sx={{backgroundColor:"#f3720fff", mt:2}}>sign up</Button>
            </Box>
         </Box>
 
          {/* img */}
-        {/* <Box sx={{ width: "8rem" }}>
+        <Box sx={{ width: "8rem" }}>
                 <Box
                   component="img"
                   sx={{
@@ -49,7 +67,7 @@ export default function Register() {
                   alt="rainbow img"
                   src={rainbowImg}
                 ></Box>
-        </Box> */}
+        </Box>
        
       </Box>
     </>
