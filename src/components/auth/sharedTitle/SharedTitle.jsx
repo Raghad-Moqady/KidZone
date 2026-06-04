86a1bf327b0739d52be6f79fabd1caac565bfa19
Raghad-Moqady <raghadmoqady@gmail.com>
2026-06-04 18:54:44 +0300

    Refactor: create shared title component for auth forms

--- src/components/auth/sharedTitle/SharedTitle.jsx
+++ src/components/auth/sharedTitle/SharedTitle.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 export default function SharedTitle({title}) {
   return (
    <>
-      <Typography variant='h4' component={"h1"} >{title}</Typography>
+       <Typography variant='h4' component={"h1"} color={{xs:"#8B5E3C",lg:"white"}}  mt={1}> {title}</Typography>
    </>
   )
 }
--- src/pages/auth/register/Register.jsx
+++ src/pages/auth/register/Register.jsx
@@ -12,6 +12,7 @@ import Logo from "../../../components/logo/Logo.jsx";
 import mainBgImg from "../../../assets/imgs/BG23.png";
 import PersonAddAltOutlinedIcon from '@mui/icons-material/PersonAddAltOutlined';
 import SharedTextField from "../../../components/auth/sharedTextField/SharedTextField.jsx";
+import SharedTitle from "../../../components/auth/sharedTitle/SharedTitle.jsx";
 
 //  steps:
 /*
@@ -70,41 +71,40 @@ export default function Register() {
       <Box display={"flex"} alignItems={"center"}  minHeight={"100vh"} justifyContent={{xs:"center",lg:"end"}}
        sx={{backgroundColor:"rgb(151, 128, 99)" ,backgroundImage:{
        xs: `linear-gradient(rgb(151, 128, 99),rgb(151, 128, 99),rgba(151, 128, 99, 0.71),rgba(151, 128, 99, 0.48), rgba(151, 128, 99, 0)), url(${mainBgImg})`},backgroundPositionY:{xs:"-122px",sm:"170px",md:"-122px"},backgroundRepeat:"no-repeat"}}>
-         {/* form card */}
-         {/* ...register('email') === id */}
-        <Box   sx={{ textAlign:"center",paddingY:{xs:7,sm:10},margin:2, width:{xs:"100%",sm:"80%",md:"60%",lg:"50%"},backgroundColor:{xs:"#ffffffcc",lg:"initial"}, borderTopLeftRadius:"20%", borderBottomRightRadius:"20%" }}>
-            <Box display={"flex"}  justifyContent={"center"} >
-               {/* <Logo/>  */}
-               <PersonAddAltOutlinedIcon sx={{color:{xs:"#8B5E3C",lg:"white"},border:1,borderRadius:"50%",width:"5rem",height:"5rem",p: 1}}/>
+          {/* form card */}
+          {/* ...register('email') === id */}
+          <Box   sx={{ textAlign:"center",paddingY:{xs:7,sm:10},margin:2, width:{xs:"100%",sm:"80%",md:"60%",lg:"50%"},backgroundColor:{xs:"#ffffffcc",lg:"initial"}, borderTopLeftRadius:"20%", borderBottomRightRadius:"20%" }}>
+              <Box display={"flex"}  justifyContent={"center"} >
+                <PersonAddAltOutlinedIcon sx={{color:{xs:"#8B5E3C",lg:"white"},border:1,borderRadius:"50%",width:"5rem",height:"5rem",p: 1}}/>
+              </Box>
+              <SharedTitle title={"Create a New Account"}/>
+            {/* to show server errors */}
+            {/* stop hear */}
+            {/* {Object.keys(serverValidationErrors).length > 0 && (
+              <Alert severity="error" sx={{ textAlign: "start", mx: 5 }}>
+                {Object.entries(serverValidationErrors).map(([field, messages]) => (
+                  <Box key={field} sx={{ mb: 1 }}>
+                    <strong>{field}:</strong>
+                    <ul style={{ margin: 0, paddingLeft: "20px" }}>
+                      {messages.map((msg, index) => (
+                        <li key={index}>{msg}</li>
+                      ))}
+                    </ul>
+                  </Box>
+                ))}
+              </Alert>
+            )} */}
+            
+            <Box component={"form"} onSubmit={handleSubmit(sendData)} display={"flex"} flexDirection={"column"} gap={2} alignItems={"center"} px={10} mt={3}>
+                <SharedTextField type={"email"} id={"email"} label={"Email"} error={errors.email} register={register('email')}/>
+                <SharedTextField type={"password"} id={"password"} label={"Password"} error={errors.password} register={register('password')}/>
+                <SharedTextField type={"text"} id={"fullName"} label={"Full Name"} error={errors.fullName} register={register('fullName')}/>
+                <SharedTextField type={"text"} id={"userName"} label={"User Name"} error={errors.userName} register={register('userName')}/>
+                <SharedTextField type={"text"} id={"phone"} label={"Phone"} error={errors.phoneNumber} register={register('phoneNumber')}/>
+
+                <Button type="submit" variant="contained"  endIcon={<SendIcon/>} sx={{backgroundColor:"#E47221", mt:2, borderRadius:50}}>sign up</Button>
             </Box>
-           <Typography variant='h4' component={"h1"} color={{xs:"#8B5E3C",lg:"white"}}  mt={1}>Sign Up</Typography>
-           {/* to show server errors */}
-           {/* stop hear */}
-           {Object.keys(serverValidationErrors).length > 0 && (
-            <Alert severity="error" sx={{ textAlign: "start", mx: 5 }}>
-              {Object.entries(serverValidationErrors).map(([field, messages]) => (
-                <Box key={field} sx={{ mb: 1 }}>
-                  <strong>{field}:</strong>
-                  <ul style={{ margin: 0, paddingLeft: "20px" }}>
-                    {messages.map((msg, index) => (
-                      <li key={index}>{msg}</li>
-                    ))}
-                  </ul>
-                </Box>
-              ))}
-            </Alert>
-          )}
-          
-           <Box component={"form"} onSubmit={handleSubmit(sendData)} display={"flex"} flexDirection={"column"} gap={2} alignItems={"center"} px={10} mt={3}>
-              <SharedTextField type={"email"} id={"email"} label={"Email"} error={errors.email} register={register('email')}/>
-              <SharedTextField type={"password"} id={"password"} label={"Password"} error={errors.password} register={register('password')}/>
-              <SharedTextField type={"text"} id={"fullName"} label={"Full Name"} error={errors.fullName} register={register('fullName')}/>
-              <SharedTextField type={"text"} id={"userName"} label={"User Name"} error={errors.userName} register={register('userName')}/>
-              <SharedTextField type={"text"} id={"phone"} label={"Phone"} error={errors.phoneNumber} register={register('phoneNumber')}/>
-              
-              <Button type="submit" variant="contained"  endIcon={<SendIcon/>} sx={{backgroundColor:"#E47221", mt:2, borderRadius:50}}>sign up</Button>
-           </Box>
-        </Box> 
+          </Box> 
       </Box>
     </>
   );
