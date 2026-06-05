10370790ec0b0f8e42862defae255f085a5e8437
Raghad-Moqady <raghadmoqady@gmail.com>
2026-06-05 23:16:37 +0300

    Register Page: handle errors(faild response)|server side validation

--- src/pages/auth/register/Register.jsx
+++ src/pages/auth/register/Register.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import style from "./Register.module.css";
 import zebraImg from "../../../assets/imgs/zebra.webp";
 import rainbowImg from "../../../assets/imgs/rainbow.png";
-import { Alert, Box, Button, Card, TextField, Typography } from "@mui/material";
+import { Box, Button, Card, TextField, Typography } from "@mui/material";
 import SendIcon from '@mui/icons-material/Send';
 import { useForm } from "react-hook-form";
 import axios from "axios";
@@ -22,14 +22,14 @@ import { ErrorToast, SuccessToast } from "../../../toast/Toast.js";
 2. (form handling)handle form using react-hook-form , download axios =>Done
 3. client side validation using Yup (level 1) (before send request)=> Done
 
-change auth button style when it disabled
+change auth button style when it disabled=>Done
 create env file=> Done
-handling isSubmitting (add loader button when request is submiting)
-handel if response.status is success 
+handling isSubmitting (add loader button when request is submiting)=>Done
+handel if response.status is success =>Done
 
-4. server side validation using try, catch (without using react query) (after send request)
-5. loading 
-6. Alerts
+4. server side validation (level 2)using try, catch (without using react query) (after send request)=>done
+5. loading =>Done
+6. toast=> Done
 *//////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 //Client Validation Steps(Yup): 1. create schema(import) , 2. validate schema (yup with ract hook form )(yup resolver)
@@ -37,13 +37,9 @@ handel if response.status is success
 // submitمعناها انه الهوك فورم رح يشغل السكيما تلقائيا عند كل عملية 
 //errors وأي خطأ بطلع مع الهوك فورم ... بروح يضيفه على ال
  
- 
 
 export default function Register() {
   const navigate= useNavigate();
-  const[serverValidationErrors,setServerValidationErrors]= useState({});
-  const[serverLogicErrors,setServerLogicErrors] =useState([]);
-  const[serverError,setServerError] =useState("");
 
   const {register, handleSubmit, formState:{errors,isSubmitting}} =useForm({
     resolver: yupResolver(RegisterSchema),
@@ -53,34 +49,79 @@ export default function Register() {
   });
 
   const sendData=async(values)=>{
-      //  console.log(values);
        try{
          const response= await axios.post(`${import.meta.env.VITE_BURL}/auth/Account/Register`,values);
-        //  console.log(response);
          if(response.status==200){
            SuccessToast("Account created successfully! Please confirm your email, then log in.");
            navigate("/auth/login");
          }
-       }catch(err){
-        // // console.log(err.response.status);
-        // //Validation (level 2): errors from backend (400,401,...,500)or from send operation (internet)
-        // // بالترتيب هاد الايرور ممكن يكون راجع من مرحلة الفالديشن 
-        // if(err.response.data.title){
-        //   setServerValidationErrors(err.response.data.errors); 
-        // }
-        // //او من السيرفر => 500 : هاد الخطأ بكون راجع من الاكسبشن هاندلر بالباك
-        // else if(err.response.status==500){
-        //   setServerError(err.response.data.message);
-        // }
-        // //او من الاخطاء التي تحدث بالservice =>try بال400,404.. (logic errors)
-        // else if(err.response.data.message){
-        //    setServerLogicErrors(err.response.data.errors)
-        // }
-       ErrorToast("Error");
+       }catch (err) {
+        //1.network error
+        //2. 400: 1.validation response from asp 2.BaseResopnse 
+        //3. 500 
+            const response = err.response;
 
-       }
+            // 1. No response (network error)
+            if (!response) {
+              ErrorToast("Network error. Please try again.");
+              return;
+            }
+ 
+            // 2. Validation & logic errors (400)
+            if (response.status === 400) {
+              const data = response.data;
+              //validation: false
+              //logic: true
+              if(data?.unexpectedErrorFlag==false){
+                //logic: 
+//     "success": false,
+//     "unexpectedErrorFlag": false,
+//     "message": "User Creation Failed",
+//     "errors": [
+//         "Username 'Eng.Raghad' is already taken.",
+//         "Email 'raghadmoqady@gmail.com' is already taken."
+//     ]
+// }
+                 if(data?.errors.length!==0){
+                   data.errors.forEach(err=> ErrorToast(err));
+                 }
+                 return;
+              }else{
+                //validation: 
+//     "type": "https://tools.ietf.org/html/rfc9110#section-15.5.1",
+//     "title": "One or more validation errors occurred.",
+//     "status": 400,
+//     "errors": {
+//         "FullName": [
+//             "Full Name Is Required"
+//         ],
+//         "UserName": [
+//             "UserName Is Required",
+//             "username must be at least 4 characters"
+//         ]
+//     },
+//     "traceId": "00-646801cbd122c9b377b5bbf8161cb72b-aa7f4f3bcebac8ed-00"
+// }
+               if (data?.errors) {
+                // errors object (field-based)
+                Object.values(data.errors).forEach((messages) => {
+                  messages.forEach((msg) => ErrorToast(msg));
+                });
+              } 
+              return;
+              }
+            }
+ 
+            // 3. Server errors (500)
+            if (response.status >= 500) {
+              ErrorToast("Server error. Please try later.");
+              return;
+            }
+            
+            // fallback
+            ErrorToast("Something went wrong");
+          }
   }
-  // ,md:`url(${mainBgImg})`
   return (
     <> 
       <Box display={"flex"} alignItems={"center"}  minHeight={"100vh"} justifyContent={{xs:"center",lg:"end"}}
@@ -93,22 +134,7 @@ export default function Register() {
                 <PersonAddAltOutlinedIcon sx={{color:{xs:"#8B5E3C",lg:"white"},border:1,borderRadius:"50%",width:"5rem",height:"5rem",p: 1}}/>
               </Box>
               <SharedTitle title={"Create a New Account"}/>
-            {/* to show server errors */}
-            {/* stop hear */}
-            {/* {Object.keys(serverValidationErrors).length > 0 && (
-              <Alert severity="error" sx={{ textAlign: "start", mx: 5 }}>
-                {Object.entries(serverValidationErrors).map(([field, messages]) => (
-                  <Box key={field} sx={{ mb: 1 }}>
-                    <strong>{field}:</strong>
-                    <ul style={{ margin: 0, paddingLeft: "20px" }}>
-                      {messages.map((msg, index) => (
-                        <li key={index}>{msg}</li>
-                      ))}
-                    </ul>
-                  </Box>
-                ))}
-              </Alert>
-            )} */}
+           
             
             <Box component={"form"} onSubmit={handleSubmit(sendData)} display={"flex"} flexDirection={"column"} gap={2} alignItems={"center"} px={10} mt={3}>
                 <SharedTextField type={"email"} label={"Email"} error={errors.email} register={register('email')}/>
@@ -116,8 +142,7 @@ export default function Register() {
                 <SharedTextField type={"text"} label={"Full Name"} error={errors.fullName} register={register('fullName')}/>
                 <SharedTextField type={"text"} label={"User Name"} error={errors.userName} register={register('userName')}/>
                 <SharedTextField type={"text"} label={"Phone"} error={errors.phoneNumber} register={register('phoneNumber')}/>
-
-
+ 
                 {isSubmitting? 
                  <Button loading variant="outlined" loadingPosition="end" endIcon={<SendIcon/>}
                  sx={{backgroundColor:"#7445247a", mt:2, borderRadius:50,px:8,pt:1,pb:1}}
@@ -131,8 +156,7 @@ export default function Register() {
                     }}}
                 disabled={errors.email||errors.password||errors.fullName||errors.userName||errors.phoneNumber}
                 >Sign Up </Button>
-                }
-              
+                } 
             </Box>
           </Box> 
       </Box>
--- src/toast/Toast.js
+++ src/toast/Toast.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import { Bounce, toast } from 'react-toastify';
 
-export  function ErrorToast(message) { 
-        toast.error(message, {
-        position: "top-right",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: false,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "light",
-        transition: Bounce,
-      });
+export function ErrorToast(message) { 
+  toast.error(message, {
+    position: "top-right",
+    autoClose: false,
+    hideProgressBar: false,
+    closeOnClick: false,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "light",
+    transition: Bounce,
+  });
 }
 export  function SuccessToast(message) { 
         toast.success(message, {
