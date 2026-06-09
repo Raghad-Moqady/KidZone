21a684b88878e08ca36eed59c563ccd25be46edb
Raghad-Moqady <raghadmoqady@gmail.com>
2026-06-09 14:11:20 +0300

    Hero Section: responsive design for all screens(xs,sm,md,lg,xl)=> Done

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -13,7 +13,7 @@ export default function Hero() {
       backgroundPosition:{xs:'right',sm:'top'},
       backgroundRepeat: "no-repeat",
       backgroundAttachment: "fixed",
-      display:'flex', alignItems:'center', justifyContent:{xs:'center'},
+      display:'flex', alignItems:'center', justifyContent:{xs:'center',sm:'unset'},
         "&::before": {
       content: '""',
       position: "absolute",
@@ -33,17 +33,18 @@ export default function Hero() {
     }}
       >
         <Box sx={{
-          textAlign:{xs:'center'},
+          textAlign:{xs:'center',sm:'start'},
           position: "relative",
-          zIndex: 2,}}>
-           <Box sx={{textAlign:{xs:'-webkit-center'}}}>
-              <Logo  width={{xs:"11rem",sm:"13rem"}} theme='lightBg'/>
+          zIndex: 2,
+          marginLeft:{sm:4,md:12,lg:20}}}>
+           <Box display={'flex'} justifyContent={{xs:'center',sm:'start'}}>
+              <Logo  width={{xs:"11rem",sm:"12rem",md:"12.5rem",lg:"13rem"}} theme='lightBg'/>
            </Box>
            
-          <Typography component={'h1'} sx={{fontWeight:'bold',fontSize:{xs:'40px',sm:'60px'},color:{xs:'#ffffff',sm:'#8B5E3C'},mt:1}}>
+          <Typography component={'h1'} sx={{fontWeight:'bold',fontSize:{xs:'40px',sm:'45px',md:'50px',lg:'60px'},color:{xs:'#ffffff',sm:'#8B5E3C'},mt:1}}>
             Everything Your <br/>Child Needs
             </Typography>  
-          <Typography component={'p'} sx={{fontSize:{xs:'20px',sm:'24px'},color:{xs:'#ffffff',sm:'#8B5E3C'},mt:1}} >
+          <Typography component={'p'} sx={{fontSize:{xs:'20px',sm:'22px',md:'23px',lg:'24px'},color:{xs:'#ffffff',sm:'#8B5E3C'},mt:1}} >
             Clothes, Toys, Learning & More
           </Typography>
           <Button type="submit" variant="contained"  endIcon={<SendIcon/>} sx={{fontSize:{xs:'14px',sm:'16px'}, backgroundColor:"#E47221",px:7,py:1, mt:2, borderRadius:50}}>shop Now</Button>
