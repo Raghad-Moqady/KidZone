c1e26dcf748438135adcc46e7e029b9112bbf42a
Raghad-Moqady <raghadmoqady@gmail.com>
2026-06-09 13:22:38 +0300

    Hero Section: responsive design for extra-small screens=> Done

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -8,24 +8,46 @@ export default function Hero() {
   return (
      <Box sx={{
       backgroundImage:`url(${heroImg})`,
-      minHeight: "88vh",
+      minHeight: "80vh",
       backgroundSize: "cover" ,
-      backgroundPosition:"start",
+      backgroundPosition:{xs:'right',sm:'top'},
       backgroundRepeat: "no-repeat",
-      display:'flex', alignItems:'center'}}
+      backgroundAttachment: "fixed",
+      display:'flex', alignItems:'center', justifyContent:{xs:'center'},
+        "&::before": {
+      content: '""',
+      position: "absolute",
+      right:0,left:0,
+      minHeight:'80vh',
+      backgroundColor: {
+        xs: "#171616a1",
+        sm: "transparent",
+      },
+      backdropFilter: {
+        xs:"blur(4px)",
+        sm:'none'
+      },
+      zIndex: 1,
+    },
+     
+    }}
       >
-        <Container sx={{}}>
+        <Box sx={{
+          textAlign:{xs:'center'},
+          position: "relative",
+          zIndex: 2,}}>
+           <Box sx={{textAlign:{xs:'-webkit-center'}}}>
+              <Logo  width={{xs:"11rem",sm:"13rem"}} theme='lightBg'/>
+           </Box>
            
-          <Logo width={"13rem"} theme='lightBg'/>
-           
-          <Typography component={'h1'}  variant='h2' sx={{fontWeight:'bold',color:'#8B5E3C',mt:1}}>
+          <Typography component={'h1'} sx={{fontWeight:'bold',fontSize:{xs:'40px',sm:'60px'},color:{xs:'#ffffff',sm:'#8B5E3C'},mt:1}}>
             Everything Your <br/>Child Needs
             </Typography>  
-          <Typography component={'p'} variant='h5' sx={{color:'#8B5E3C',mt:1}} >
+          <Typography component={'p'} sx={{fontSize:{xs:'20px',sm:'24px'},color:{xs:'#ffffff',sm:'#8B5E3C'},mt:1}} >
             Clothes, Toys, Learning & More
           </Typography>
-          <Button type="submit" variant="contained"  endIcon={<SendIcon/>} sx={{fontSize:'16px', backgroundColor:"#E47221",px:7,py:1, mt:2, borderRadius:50}}>shop Now</Button>
-        </Container>
+          <Button type="submit" variant="contained"  endIcon={<SendIcon/>} sx={{fontSize:{xs:'14px',sm:'16px'}, backgroundColor:"#E47221",px:7,py:1, mt:2, borderRadius:50}}>shop Now</Button>
+        </Box>
     </Box>
   )
 }
