bcd26cc90d35b81171660e3f6a2e1b5fb991c4ab
Raghad-Moqady <raghadmoqady@gmail.com>
2026-07-03 23:54:42 +0300

    Products: add (More Products) Button to Products Component

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -5,7 +5,17 @@ import productImg2 from "../../assets/imgs/productsImgs/product2.webp";
 import productImg3 from "../../assets/imgs/productsImgs/product3.webp";
 import productImg4 from "../../assets/imgs/productsImgs/product4.webp";
 
-import { Box, Button, Card, CardActions, CardContent, CardMedia, Container, Grid, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Container,
+  Grid,
+  Typography,
+} from "@mui/material";
 import SectionTitle from "../shared/SectionTitle";
 import Item from "@mui/material/Grid";
 import ProductCard from "../shared/ProductCard";
@@ -32,27 +42,73 @@ export default function Products() {
         <Grid container spacing={3}>
           <Grid size={{ xs: 12, sm: 6, md: 3 }}>
             <Item>
-              <ProductCard mainImage={productImg1} name="Happy Dino Toy" categoryName="Toys" status="Active" discount='0' isNew={true} price='300'/>
+              <ProductCard
+                mainImage={productImg1}
+                name="Happy Dino Toy"
+                categoryName="Toys"
+                status="Active"
+                discount="0"
+                isNew={true}
+                price="300"
+              />
             </Item>
           </Grid>
-           <Grid size={{ xs: 12, sm: 6, md: 3 }}>
+          <Grid size={{ xs: 12, sm: 6, md: 3 }}>
             <Item>
-              <ProductCard mainImage={productImg2} name="Happy Dino Toy" categoryName="Toys" status="InActive" discount='10' isNew={true} price='100'/>
+              <ProductCard
+                mainImage={productImg2}
+                name="Happy Dino Toy"
+                categoryName="Toys"
+                status="InActive"
+                discount="10"
+                isNew={true}
+                price="100"
+              />
             </Item>
           </Grid>
-           <Grid size={{ xs: 12, sm: 6, md: 3 }}>
+          <Grid size={{ xs: 12, sm: 6, md: 3 }}>
             <Item>
-              <ProductCard mainImage={productImg3} name="Happy Dino Toy" categoryName="Toys" status="Active" discount='11' isNew={true} price='50'/>
+              <ProductCard
+                mainImage={productImg3}
+                name="Happy Dino Toy"
+                categoryName="Toys"
+                status="Active"
+                discount="11"
+                isNew={true}
+                price="50"
+              />
             </Item>
           </Grid>
-           <Grid size={{ xs: 12, sm: 6, md: 3 }}>
+          <Grid size={{ xs: 12, sm: 6, md: 3 }}>
             <Item>
-              <ProductCard mainImage={productImg4} name="Happy Dino Toy" categoryName="Toys" status="Active" discount='50' isNew={true} price='1000'/>
+              <ProductCard
+                mainImage={productImg4}
+                name="Happy Dino Toy"
+                categoryName="Toys"
+                status="Active"
+                discount="50"
+                isNew={true}
+                price="1000"
+              />
             </Item>
           </Grid>
-          
-          
         </Grid>
+
+        {/* Show All Products Button */}
+        <Box sx={{textAlign:'center', marginTop:4}}>
+          <Button
+            variant="contained"
+            sx={{
+              fontSize: { xs: "14px", sm: "16px" },
+              backgroundColor: "#579754",
+              px:7,py:1,
+              borderRadius: 50,
+              boxShadow: 0,
+            }}
+          >
+             More Products
+          </Button>
+        </Box>
       </Container>
     </Box>
   );
